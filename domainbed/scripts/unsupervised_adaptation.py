a54d4bb667dedd75f445b5a70b8cea209088645f
Yi-Fan Zhang <yifanzhang.cs@gmail.com>
2023-05-03 17:39:09 +0800

    adanpc

--- domainbed/scripts/unsupervised_adaptation.py
+++ domainbed/scripts/unsupervised_adaptation.py
@@ -377,7 +377,8 @@ if __name__ == "__main__":
         adapt_hparams_dict = {
             'beta': [0.0, 0.25, 0.75,  1.1],
             'k': [1, 5],
-            'eps_ball': [0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.5],# an increasing eps?0.95, 0.9,
+            'gamma': [1, 3], 
+            'alpha': [0.1, 1.0, 10.0],
             'temperature': [0.01, 0.05, 0.1, 0.25, 0.5]
         }
     elif args.adapt_algorithm in ['SHOT', 'SHOTIM']:
